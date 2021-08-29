97a5ad74daa5be34d690aa33fb7999e96fe9b722
sandeep <xhrsdon@gmail.com>
2021-08-29 10:30:33 +0545

    before adding css module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import About from './components/About/About';
 import Navbar from './components/Navbar/Navbar';
-import TextForm from './components/TextForm/TextForm';
+// import TextForm from './components/TextForm/TextForm';
 
 function App() {
   return (
    <div>
      <Navbar title="TextUtils" />
-     <TextForm heading="Enter your text here!" />
+     {/* <TextForm heading="Enter your text here!" /> */}
+     <About />
    </div>
   );
 }
